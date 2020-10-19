c641698eef544645f6deab3a76bee13002cc443a
Neo <neo.lee@kakao.com>
2020-10-19 23:12:12 +0900

    Log Out & Navigation

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { authService } from 'fbase';
+import { auth } from 'firebase';
 
-const Home = () => <span>Home</span>;
+const Home = () => <div>Welcome</div>;
+
+console.log(authService.currentUser);
 
 export default Home;
